c6bba3d0945b4b046a98d688c07c680da11f5de7
ttrmoreira <ttrmoreira@gmail.com>
2018-11-27 08:57:32 -0200

    Alteração do método get adicionando a api fetch do ES2016 que reduz a quantidade de código necessário para manipular o XMLHttpRequest. Porém o uso da api fetch não permite a interrupção ou interferência na requisição por adotar um scafolding.

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,48 +1,19 @@
 class HttpService{
 
+	_handleErrors(res){
+		if(!res.ok) throw new Error(res.statusText);
+		return res;
 
-	get(url){
-				return new Promise((resolve, reject) =>{
-
-			let xhr = new XMLHttpRequest();
-			
-			/*Configurações
-			
-				0: requisição ainda não iniciada
-
-				1: conexão com o servidor estabelecida
-
-				2: requisição recebida
-
-				3: processando requisição
-
-				4: requisição está concluída e a resposta está pronta
-
-
-			*/
-			
-			xhr.open('GET', url);
-			console.log(xhr.readyState);
-
-			xhr.onreadystatechange = () => {
-				if(xhr.readyState == 4){
-					if(xhr.status == 200){
-
-						resolve(JSON.parse(xhr.responseText));
-	            	} else {
-	            		
-	            		console.log(xhr.responseText);
-	            		reject(xhr.responseText);
-
-	            	}
-				}
-			};
-			
-			/*Executa*/
-			xhr.send();
-
-		});
 	}
+	
+	get(url){
+
+		return fetch(url)
+			.then(res => this._handleErrors(res))
+			.then(res => res.json());
+	}
+
+
 
 
 
